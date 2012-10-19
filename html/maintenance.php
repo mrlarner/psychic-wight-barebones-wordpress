e54f76b4ba85240ac8cf51e613993f67b6adaba0
Matt Larner <mrlarner@gmail.com>
2012-10-19 15:24:09 -0700

    added base docroot

---
+++ html/maintenance.php
@@ -0,0 +1,27 @@
+<style type="text/css">
+	body {
+		font-family: 'Helvetica Neue', Arial, Helvetica, sans-serif;
+		margin: 0 auto;
+		text-align: center;
+		height: 100%;
+		width: 100%;
+	}
+	
+	p {
+		background: #333;
+		font-size: 120%;
+		line-height: 1.2em;
+		padding: 3em 0;
+		position: absolute;
+		bottom: 25%;
+		width: 100%;
+		color: #fff;
+	}
+	
+	::selection {
+		background: #000;
+		color: #fff;
+	}
+</style>
+
+<p>We are currently performing scheduled maintenance and will be back online shortly.</p>
